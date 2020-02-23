aaa5b6c976da393b716a355c9b492279c6541358
garyhuangdev <jiazhengsang@gmail.com>
2020-02-23 23:11:24 +1100

    Updates

---
+++ precache-manifest.b5d44fff8c7c3e82052bc17a5a1c732e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c2ffec24cb908597a767ed85bf447a",
+    "url": "/weather-widget/index.html"
+  },
+  {
+    "revision": "26b5e490d0952aa8b150",
+    "url": "/weather-widget/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "1b6ff5b8f270fdbe83b3",
+    "url": "/weather-widget/static/css/main.c7193bcf.chunk.css"
+  },
+  {
+    "revision": "26b5e490d0952aa8b150",
+    "url": "/weather-widget/static/js/2.d0ac3f18.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/weather-widget/static/js/2.d0ac3f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6ff5b8f270fdbe83b3",
+    "url": "/weather-widget/static/js/main.0c609667.chunk.js"
+  },
+  {
+    "revision": "c668feb4917dcfdc679c",
+    "url": "/weather-widget/static/js/runtime-main.680f2098.js"
+  },
+  {
+    "revision": "445f76bba2d3159e971fd262b9f11028",
+    "url": "/weather-widget/static/media/spinner.445f76bb.gif"
+  }
+]);
